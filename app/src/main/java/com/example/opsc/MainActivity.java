e34c74571a7bc9b4e52820041cabc517b653df62
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-09-29 18:32:55 +0200

    Select Tour, Add landmark and Navigation bar pages

--- app/src/main/java/com/example/opsc/MainActivity.java
+++ app/src/main/java/com/example/opsc/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.opsc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // as soon as the application opens the first
+        // fragment should be shown to the user
+        // in this case it is algorithm fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SelectTour()).commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.Dashboard_Nav) {
+            selectedFragment = new SelectTour();
+        }
+        else if (itemId == R.id.Camera_Nav) {
+            selectedFragment = new AddLandmark();
+        }
+/*        else if (itemId == R.id.profile) {
+            selectedFragment = new ProfileFragment();
+        }*/
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
 }
+
+
