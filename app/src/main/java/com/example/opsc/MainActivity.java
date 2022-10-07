968f1d4086548d006ec1536788bdf25d3a370ec3
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-10-07 14:13:48 +0200

    Attempted to get users current location on map

--- app/src/main/java/com/example/opsc/MainActivity.java
+++ app/src/main/java/com/example/opsc/MainActivity.java
@@ -2,11 +2,26 @@ package com.example.opsc;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
@@ -75,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
         });
         //End of places autocomplete fragment code.
         //====================================================================================
-
 
     }
 }
