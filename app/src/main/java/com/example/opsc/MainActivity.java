e2c9ad78ff927954308ecadd6592d7c9ebe78cc7
ST10119495 <ST10119495@vcconnect.co.za>
2022-10-16 13:42:33 +0200

    Connected Home Page to Login and Register Pages

--- app/src/main/java/com/example/opsc/MainActivity.java
+++ app/src/main/java/com/example/opsc/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.opsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaration of variables ()
+    Button LoginButton;
+    Button RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assigning buttons ()
+        LoginButton = (Button) findViewById(R.id.loginFrag);
+        RegisterButton = (Button) findViewById(R.id.registerFrag);
+
+        // The following listener will redirect the user to the dashboard ()
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Dashboard.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        // The following listener will redirect the user to the register page ()
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegisterFragment.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
     }
+
+
 }
